00b1f608dd7bf96c775b13e88d99a1a76567754e
Josh West <josh.west@centre.edu>
2019-11-18 17:30:27 -0500

    done

--- javascript.js
+++ javascript.js
@@ -20,6 +20,10 @@ penguinPromise.then
 //set up svg, scales, and axes
 var setup = function(penguins,index)
 {
+    //print index to console
+    console.log("index:",index)
+    
+    //make svg
     d3.select("svg")
     .attr("width",screen.width)
     .attr("height",screen.height)
@@ -27,12 +31,33 @@ var setup = function(penguins,index)
     .attr("id","graph")
     .attr("transform","translate("+margins.left+","+margins.top+")")
     //tooltip stuff
-        .on("mouseover", function(quizes, day)
+    .on("mouseover", function(quiz, index)
     {  
-       d3.select("#tooltip").style("left",(d3.event.pageX+20)+"px").style("top",(d3.event.pageY-25)+"px").text("("+day+","+quizes+")")
-    }
-    )
-    
+        console.log("inside on mouseover")//working
+        //console.log("quiz",penguins[index].quizes[index].grade)//FIXME: figure out how to get the grade info for each circle
+        d3.select("#tooltip").classed("hidden", false)
+        d3.select("body").append("span").attr("id","tooltip") //creates tooltip span every time
+        d3.select("#tooltip")
+            .style("left",(d3.event.pageX+20)+"px")
+            .style("top",(d3.event.pageY-0)+"px")
+            .style("width", 150 +"px")
+            .style("height",25+"px")
+            .style("border-radius", 10+"px")
+            .style("position","fixed")
+            .style("background-color","grey")
+            .style("pointer-event","none")
+            //.style("font-size",15+"px")
+            //.style("border", 10+"px"+10+"px"+"black")
+            //.style("box-shadow",4+"px" 4+"px" 10+"px"+ "right"+ "rgba(0,0,0,0.4)")
+            .text("(Tip: penguin data)") //tooltip not responding to CSS and printing wrong index
+            //.style("text-align", "center")
+            
+    })
+    .on("mouseout", function()
+    {
+        console.log("inside on mouseout")//not working
+        d3.select("#tooltip").classed("hidden",true)    
+    })
     var width = screen.width - margins.left - margins.right
     var height = screen.height - margins.top - margins.bottom
     
@@ -66,12 +91,12 @@ var setup = function(penguins,index)
     //draw the circles for the first time
     var initialCircles = d3.select("#graph")
         .selectAll("circle")
-        .data(penguins[index].quizes)//FIXME: problem with index here
+        .data(penguins[index].quizes)
         .enter()
         .append("circle")
         .attr("fill", function(colors) //different color for each penguin
         {
-            console.log("index",index); console.log("penguins",penguins); return cScale(penguins[index].picture)     
+            return cScale(penguins[index].picture)     
         })
         .attr("cx",function(quiz,day) //x positions of circles
         {
@@ -81,7 +106,7 @@ var setup = function(penguins,index)
         {
             return yScale(quiz.grade)
         })
-        .attr("r",5)
+        .attr("r",10)
     
     drawPenguins(penguins,xScale,yScale,cScale,0);
     makeButtons(penguins,xScale,yScale,cScale); //this was previously in the promise (which was wrong bc xScale, yScale, etc. from setup couldn't be used)
@@ -92,14 +117,15 @@ var drawPenguins = function(penguins,xScale,yScale,cScale,index)
     var groups = d3.select("#graph")
         .selectAll("circle")
         .data(penguins[index].quizes)
-        //.enter()
-        //.append("circle")
+        //.enter() <-- don't need this here since there are already visual elements
+        //.append("circle") <-- don't need this here since there are already circles
         //put transition() here
         //don't remove()
         .transition()
+        .duration(1000)
         .attr("fill", function(colors) //different color for each penguin
         {
-            console.log("index",index); console.log("penguins",penguins); return cScale(penguins[index].picture)     
+            return cScale(penguins[index].picture)     
         })
         .attr("cx",function(quiz,day) //x positions of circles
         {
@@ -109,7 +135,11 @@ var drawPenguins = function(penguins,xScale,yScale,cScale,index)
         {
             return yScale(quiz.grade)
         })
-        .attr("r",5)   
+        .attr("r",10)
+    //make dynamic title for graph
+    d3.select("span")
+    .attr("id","title")
+    .text("Penguin " + index + "'s Quiz Grades") //index changes to the correct penguin clicked
 }
 
 var makeButtons = function(penguins, xScale, yScale, cScale)
